df54ce9d25d2a5873e19a6f62499726eaaf53db0
bob3rt <rmcombs24@gmail.com>
2019-02-01 17:43:51 -0500

    First Commit of my Mass Media editor. Basic UI, and multiselect add.

---
+++ Windows/MassMediaEditor/MassMediaEditor/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+
+namespace MassMediaEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Multiselect = true;
+            
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                var fileContent = string.Empty;
+
+                lstvInfoBox.IsEnabled = true;
+                lstvInfoBox.Items.Clear();
+
+                string filePath = dlg.FileName;
+                var file = ShellFile.FromFilePath(filePath);
+
+                List<String> ImageHeaders = new List<string>(new string[] {"File Name" ,"Title", "Subject", "Rating", "Tags", "Comments", "Authors", "Date Taken", "Program Name", "Date Aquired", "Copyright" });
+
+                for (int index = 0; index < ImageHeaders.Count; index++)
+                {
+                    if (((GridView)lstvInfoBox.View).Columns.Count <= index)
+                    {
+                        GridViewColumn newCol = new GridViewColumn();
+                        newCol.Width = double.NaN; //Auto-size the width of the columns
+
+                        //Next
+                        //newCol.DisplayMemberBinding
+
+                        ((GridView)lstvInfoBox.View).Columns.Add(newCol);
+                    }
+
+                    ((GridView)lstvInfoBox.View).Columns[index].Header = ImageHeaders[index];
+                }
+
+                // Set the list view to the opened files
+                lstvInfoBox.ItemsSource = dlg.SafeFileNames;
+
+                
+            }
+        }
+
+        private void RdoPictures_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RdoVideo_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RdoAudio_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Windows/MassMediaEditor/MassMediaEditor/Media.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassMediaEditor
+{
+    class Media
+    {
+        /* These are shared by all media types
+        **** Description
+        * Title 
+        * Subject
+        * Rating
+        * Tags
+        * Comments
+        */
+        
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public String Title { get; set; }
+        public String Subject { get; set; }
+        public String Comments { get; set; }
+        public double Rating { get; set; }
+        public List<String> Tags { get; set; }
+    }
+
+    class Pictures : Media
+    {
+        /* Editable Extened properties for Images (This does not include Camera/Photo settings)
+         **** Origin
+         * Authors
+         * Date Taken
+         * Program Name
+         * Date Aquired
+         * Copyright
+         */
+
+        public List<string> Authors { get; set; }
+        public String ProgramName { get; set; }
+        public DateTime DateAquired { get; set; }
+        public DateTime DateTaken { get; set; }
+        public double Copyright { get; set; }
+    }
+
+    class Audio : Media
+    {
+
+    }
+
+    class Video : Media
+    {
+
+    }
+}
