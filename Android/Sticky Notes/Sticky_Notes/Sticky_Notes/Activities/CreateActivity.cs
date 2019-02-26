f81b919325297b703eed8bad0ce7fed5fc2fb232
Bob <rmcombs24@gmail.com>
2019-02-25 19:39:56 -0500

    First commit of Sticky notes to it's new folder in the Projects folder. This is the first actual commit of the project now that I am using github. (Started Nov '18)

---
+++ Android/Sticky Notes/Sticky_Notes/Sticky_Notes/Activities/CreateActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Sticky_Notes
+{
+    [Activity(Label = "CreateActivity")]
+    public class CreateActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.create_note);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.tb_create);
+            Button btnSave =  FindViewById<Button>(Resource.Id.btn_save);
+            Button btnClear = FindViewById<Button>(Resource.Id.btn_clr);
+           
+
+            btnSave.Click += BtnSave_Click;
+            btnClear.Click += BtnClear_Click;
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+
+            View view = (View)sender;
+            EditText txtNote = FindViewById<EditText>(Resource.Id.txtCreateNote);
+
+            txtNote.Text = string.Empty;
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            EditText txtNote = FindViewById<EditText>(Resource.Id.txtCreateNote);
+            Intent.PutExtra("NewNote", txtNote.Text);
+            SetResult(Result.Ok, Intent);
+            Finish();
+        }
+    }
+}
---
+++ Android/Sticky Notes/Sticky_Notes/Sticky_Notes/Activities/MainActivity.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Android.Content;
+
+namespace Sticky_Notes
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : ListActivity
+    {
+        const int CREATE_BASE_NOTE = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            //SetContentView(Resource.Layout.activity_main);
+            String[] items = new string[] { "Vegetables", "Fruits", "Flower Buds", "Legumes", "Bulbs", "Tubers" };
+            ListAdapter = new ArrayAdapter<string>(this, Resource.Layout.list_item, items);
+
+            ListView.TextFilterEnabled = true;
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+
+            //FloatingActionButton fabAddNoteMenu = FindViewById<FloatingActionButton>(Resource.Id.fabAddNoteMenu);
+            //fabAddNoteMenu.Click += OpenActionMenu_Click;
+
+            FloatingActionButton fabCreateNote = FindViewById<FloatingActionButton>(Resource.Id.fabCreateNote);
+            fabCreateNote.Click += delegate 
+            {
+                var intent = new Intent(this, typeof(CreateActivity));
+                StartActivityForResult(intent, CREATE_BASE_NOTE);
+            };
+
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            if (requestCode == CREATE_BASE_NOTE)
+            {
+                if (resultCode == Result.Ok)
+                {
+                    ArrayAdapter<TextView> textViewAdapter = new ArrayAdapter<TextView>(this, Resource.Layout.content_main);
+                    //ListView lvContentMain = FindViewById<ListView>(Resource.Id.lvNoteList);
+                    TextView NewText = new TextView(this);
+                    NewText.Text = data.GetStringExtra("NewNote");
+                    textViewAdapter.Add(NewText);
+                    textViewAdapter.NotifyDataSetChanged();
+                }
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void OpenActionMenu_Click(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Flyout Menu - Note Templates", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        }
+
+
+}
+
+}
