9d0f3f242d6871a9ffdba0c8207439bac1a65446
Bob <rmcombs24@gmail.com>
2019-05-18 19:37:12 -0400

    Initial Commit
    Initial Commit of this Division 2 Weapons calculator tool.

---
+++ Windows/Division2_WeaponDmg_Calc/WeaponModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division2_WeaponDmg_Calc
+{
+    class WeaponModel
+    {
+        public static string Family     { get; set; }
+        public static string Make       { get; set; }
+        public static string Model      { get; set; }
+
+        public int CritDistanceMin      { get; set; }
+        public int CritDistanceMax      { get; set; }
+        public int RPM                  { get; set; }
+        public int OptimalRange         { get; set; }
+        public int MagSize              { get; set; }
+        public int ReloadSpeed          { get; set; }
+        public double HSMultipler       { get; set; }
+        public int NormalizedDmg        { get; set; }
+        public int[] DamageRange        { get; set; }
+        public bool isExotic            { get; set; }
+
+        public WeaponModel()
+        {
+
+
+
+
+        }
+    }
+}
